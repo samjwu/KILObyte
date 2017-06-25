7024759a27bff6471edc0708ba282c242916548a
Sam Wu <sjwu@ualberta.ca>
2017-06-24 23:02:34 -0700

    mvt logic in prog

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-var scorelabel = document.getElementById("score");
 var divisionsinput = document.getElementById("divisions");
 var changedivisions = document.getElementById("change-divisions");
+var scorelabel = document.getElementById("score");
 
 var score = 0;
 var divisions = 4;
@@ -20,7 +20,7 @@ startgame();
 function startgame() {
     createcells();
     drawallcells();
-    newcell();
+    gencell();
 }
 
 //function to place a cell based on row/column
@@ -31,7 +31,7 @@ function cell(row, col) {
     this.y = row * width + 5 * (row + 1);
 }
 
-//function to init array of cells
+//function to init array of empty cells
 function createcells() {
     for (var i = 0; i < divisions; i++) {
         cells[i] = [];
@@ -80,7 +80,8 @@ function drawallcells() {
     }
 }
 
-function newcell() {
+//function to generate a nonempty cell with a number in it
+function gencell() {
     while (true) {
         var row = Math.floor(Math.random() * divisions);
         var col = Math.floor(Math.random() * divisions);
@@ -91,3 +92,47 @@ function newcell() {
         }
     }
 }
+
+//inline function to move cells on arrow key input
+document.onkeydown = function (event) {
+    if (!gameover) {
+        if (event.keyCode == 38 || event.keyCode == 87) { moveU(); }
+        else if (event.keyCode == 39 || event.keyCode == 68) { moveR(); }
+        else if (event.keyCode == 40 || event.keyCode == 83) { moveD(); }
+        else if (event.keyCode == 37 || event.keyCode == 65) { moveL(); }
+        scorelabel.innerHTML = "Score : " + score; //update score
+    }
+}
+
+//function to move cells up
+function moveU() {
+    for (var j = 0; j < divisions; j++) {
+        for (var i = 1; i < divisions; i++) {
+            if (cells[i][j].value) {
+                var row = i;
+                while (row > 0) {
+                    if (!cells[row-1][j].value) {
+                        cells[row-1][j].value = cells[row][j].value;
+                        cells[row][j].value = 0;
+                        row--;
+                    }
+                    else if (cells[row-1][j].value == cells[row][j].value) {
+                        // cells[row-1][j].value = 2 * cells[row-1][j].value;
+                        cells[row-1][j].value *= 2;
+                        score += cells[row-1][j].value;
+                        cells[row][j].value = 0;
+                    }
+                }
+            }
+        }
+    }
+}
+
+//function to move cells right
+function moveR() {}
+
+//function to move cells down
+function moveD() {}
+
+//function to move cells left
+function moveL() {}
